9504a729e2405b471445ac5363087005c10f1d7e
liqingbo <18895391769@163.com>
2026-03-12 16:16:31 +0800

    1：修改格式

--- inc/mb_rtu.h
+++ inc/mb_rtu.h
@@ -1,5 +1,5 @@
 /****************************************************************
- * Modbus Library for C
+ * File: mb_rtu.h 
  * Author: li
  * Description: This library provides a unified interface for Modbus communication in RTU, ASCII, and TCP modes.
  * License: MIT License
--- src/mb.c
+++ src/mb.c
@@ -1,3 +1,12 @@
+/****************************************************************
+ * File: mb.c 
+ * Modbus Library for C
+ * Author: li
+ * Description: This library provides a unified interface for Modbus communication in RTU, ASCII, and TCP modes.
+ * License: MIT License
+ * Copyright (c) 2026 Your Name
+ * *****************************************************************/
+
 #include "mb.h"
 
 //********************************************************************************
